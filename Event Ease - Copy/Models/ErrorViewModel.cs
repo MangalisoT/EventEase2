2873b801bb018d1bb8153699fe3ba7470ccc7b9d
ST10451174 <165951497+MangalisoT@users.noreply.github.com>
2025-04-03 23:33:14 +0200

    Delete Event Ease - Copy directory

--- Event Ease - Copy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Event_Ease.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
